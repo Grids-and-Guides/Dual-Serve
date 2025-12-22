9da8b0b1bd3ea25de5dc0384d929847488d9d2eb
mr-harikrishnan <mr.harikrishnan85@gmail.com>
2025-12-22 11:49:46 +0530

    Refactor appStack and authFunction formatting

--- bin/app-config.ts
+++ bin/app-config.ts
@@ -14,65 +14,64 @@ import { deleteCourseFunction } from "@/apis/examples/courses/delete/delete-cour
 
 // Create object instances
 const authFunction = new FunctionConfig({
-  name: "auth-${self.stage}",
-  runtime: "lambda.Runtime.NODEJS_22_X",
-  handler: "index.handler",
-  srcFile: "src/apis/examples/authorizer.ts",
-  output: path.resolve(process.cwd(), "dist/src/apis/examples/authorizer.js"),
-  memory: 256,
-  concurrency: 10,
-  timeout: 30,
-  environmentVariable: {
-    MONGODB_URI: "localhost:db",
-    frontendUrl: "${env.frontendUrl}",
-    functionName: "${currentFunction.name}",
-    cors: "${env.cors}"
-  }
-});
+    name: "auth-${self.stage}",
+    runtime: "lambda.Runtime.NODEJS_22_X",
+    handler: "index.handler",
+    srcFile: "src/apis/examples/authorizer.ts",
+    output: path.resolve(process.cwd(), "dist/src/apis/examples/authorizer.js"),
+    memory: 256,
+    concurrency: 10,
+    timeout: 30,
+    environmentVariable:{
+      MONGODB_URI: "localhost:db",
+      frontendUrl: "${env.frontendUrl}",
+      functionName: "${currentFunction.name}",
+      cors: "${env.cors}"
+    }
+  });
 
 export const appStack = new AppStack({
-  appName: "CDK lambda",
-  version: "1.0.0",
-  region: "ap-south-1",
-  stage: getState(Stage.Local),
-  provider: "aws",
-  envFile: {
-    "local": "../environment/.local.env",
-    "dev": "../environment/.dev.env",
-    "production": "../environment/.production.env"
-  },
-  authorizer: [new Authorizer({ name: "custom-auth", type: "restApi", authFunction: authFunction })],
-  vpc: [new Vpc({ name: "vpc-1" })],
-  apiGateway: [
-    new ApiGateway({
-      name: "my-serverless-app-${self.stage}",
-      type: "http",
-      authenticationType: "custom",
-      cors: "${env.cors}"
-
+    appName: "CDK lambda",
+    version: "1.0.0",
+    region: "ap-south-1",
+    stage: getState(Stage.Local),
+    provider: "aws",
+    envFile: {
+      "local": "../environment/.local.env",
+      "dev": "../environment/.dev.env",
+      "production": "../environment/.production.env"
+    },
+    authorizer: [new Authorizer({name: "custom-auth", type: "restApi", authFunction: authFunction})],
+    vpc: [new Vpc({name:"vpc-1"})],
+    apiGateway: [
+      new ApiGateway({
+        name: "my-serverless-app-${self.stage}",
+        type: "http",
+        authenticationType: "custom",
+        cors: "${env.cors}"
     })
-  ],
-  websocketApi: [
-    new WebsocketApi({
-      name: "my-websocket-api-${self.stage}",
-      routeSelectionExpression: "$request.body.action",
-      stageName: "${self.stage}"
-    })
-  ],
-  functions: [
-    courseGetFunction,
-    courseListFunction,
-    usersListFunction, 
-    createUserFunction,
-    getUserFunction,
-    updateUserFunction,
-    deleteUserFunction,
-    createCourseFunction,
-    updateCourseFunction,
-    deleteCourseFunction,
-  ]
-}
-);
-
-// To generate JSON
-// console.log(JSON.stringify(appStack, null, 2));
+    ],
+    websocketApi: [
+      new WebsocketApi({
+        name: "my-websocket-api-${self.stage}",
+        routeSelectionExpression: "$request.body.action",
+        stageName: "${self.stage}"
+      })
+    ],
+    functions: [
+      courseGetFunction, 
+      courseListFunction, 
+      usersListFunction, 
+      createUserFunction, 
+      getUserFunction,
+      updateUserFunction,
+      deleteUserFunction,
+      createCourseFunction,
+      updateCourseFunction,
+      deleteCourseFunction,
+    ]
+  }
+  );
+  
+  // To generate JSON
+  // console.log(JSON.stringify(appStack, null, 2));
